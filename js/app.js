6631ad48d084152db8980db91580acbba4417f5e
veetanapon <volleon28@gmail.com>
2026-01-28 10:10:23 +0700

    new ver

--- js/app.js
+++ js/app.js
@@ -1,15 +1,10 @@
 // GAS_URL should be injected server-side or via build step
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbw3sEklBDVKlGeH0ZR5ouEfBRzHSZkD5jx3-AJMmWUUC9hphn4TpkcIB7J27hkTFUI1/exec';
-//const GAS_URL = 'https://script.google.com/macros/s/AKfycbxQfcGp5gOwRJAZwHSnqLs3HZ4Im_grEqzzrHvHZ_E/dev';
-//const USER_ID = 'demo_user';
-const USER_ID = localStorage.getItem('uid');
 
 async function loadCars() {
   const res = await fetch(`${GAS_URL}?action=vehicles&uid=${USER_ID}`);//.then(r => r.json());
   const json = await res.json();
   const list = document.getElementById('carList');
   list.innerHTML = '';
-
 
   for (const v of json.vehicles) {
     const vid = v[0];
---
+++ js/config.js
@@ -0,0 +1,5 @@
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbw3sEklBDVKlGeH0ZR5ouEfBRzHSZkD5jx3-AJMmWUUC9hphn4TpkcIB7J27hkTFUI1/exec';
+//const GAS_URL = 'https://script.google.com/macros/s/AKfycbxQfcGp5gOwRJAZwHSnqLs3HZ4Im_grEqzzrHvHZ_E/dev';
+
+//const USER_ID = 'demo_user';
+const USER_ID = localStorage.getItem('uid');
